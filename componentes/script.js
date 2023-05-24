ebc4e749400e603ba9cb3a6dce282eba11903b03
Laysa Diniz <95299576+santosdlaysa@users.noreply.github.com>
2023-05-24 15:43:57 -0400

    Alterações no css e js

--- componentes/script.js
+++ componentes/script.js
@@ -1,9 +1,10 @@
 window.onload = function () {
-  
+  var horas = 00;
   var seconds = 00; 
   var tens = 00; 
-  var appendTens = document.getElementById("tens")
-  var appendSeconds = document.getElementById("seconds")
+  var appendHoras = document.getElementById("horas");
+  var appendTens = document.getElementById("tens");
+  var appendSeconds = document.getElementById("seconds");
   var buttonStart = document.getElementById('button-start');
   var buttonStop = document.getElementById('button-stop');
   var buttonReset = document.getElementById('button-reset');
@@ -22,8 +23,10 @@ window.onload = function () {
 
   buttonReset.onclick = function() {
      clearInterval(Interval);
+    horas = "00";
     tens = "00";
   	seconds = "00";
+    appendHoras.innerHTML = horas;
     appendTens.innerHTML = tens;
   	appendSeconds.innerHTML = seconds;
   }
@@ -48,10 +51,24 @@ window.onload = function () {
       appendSeconds.innerHTML = "0" + seconds;
       tens = 0;
       appendTens.innerHTML = "0" + 0;
+
     }
     
     if (seconds > 9){
       appendSeconds.innerHTML = seconds;
+    }
+
+    if (seconds > 59) {
+      console.log("horas");
+      horas++;
+      appendHoras.innerHTML = "0" + horas;
+      seconds = 0;
+      appendSeconds.innerHTML = "0" + 0;
+
+    }
+
+    if (horas > 9){
+      appendHoras.innerHTML = horas;
     }
   
   }
